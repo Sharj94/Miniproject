eed84a645c64622be5a4fc2355a61153e59c4b54
Sharjeel <sajid.sharjeel@gmail.com>
2022-11-17 19:33:32 +0000

    Remove add_product from mainproject

--- Miniproject/mainproject.py
+++ Miniproject/mainproject.py
@@ -58,9 +58,9 @@ def enumerate_product():
 
 
 def add_product():
-    Product = input("New product name: ")
-    Price = float(input("New product price: "))
-    new_product = {"Product": Product, "Price": Price}
+    product = input("New product name: ")
+    price = float(input("New product price: "))
+    new_product = {"Product": product, "Price": price}
     product_list.append(new_product)
 
 
