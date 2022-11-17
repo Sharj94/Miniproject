1ce568ec574bfcfad38d34c549267e90f247da85
Sharjeel <sajid.sharjeel@gmail.com>
2022-11-17 19:31:27 +0000

    Create test_product_addition

--- Miniproject/tests/test_functions.py
+++ Miniproject/tests/test_functions.py
@@ -1,13 +1,26 @@
-from mainproject import add_product
+from unittest import mock
+
+import pytest
+
+from product import add_product
 
 
-def test_product_addition():
-    #set up
-    product = add_product()
+@pytest.fixture
+def product_list_with_no_products():
+    return []
 
 
-    #act
+def test_product_addition(product_list_with_no_products):
+    # set up
+    mock_product = "Apple"
+    mock_price = "5000"
 
+    expected_outcome = [{"Product": "Apple", "Price": 5000.0}]
 
-    #assert
-    
+    # act
+    mock_args = [mock_product, mock_price]
+    with mock.patch("builtins.input") as mock_input:
+        mock_input.side_effect = mock_args
+        assert product_list_with_no_products == []
+        add_product(product_list_with_no_products)
+        assert product_list_with_no_products == expected_outcome
