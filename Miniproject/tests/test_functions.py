a6c95d49d4bce2a10fdd1c843517683f2dacce7c
Sharjeel <sajid.sharjeel@gmail.com>
2022-11-18 15:16:08 +0000

    Added new tests - need to be debugged

--- Miniproject/tests/test_functions.py
+++ Miniproject/tests/test_functions.py
@@ -2,7 +2,7 @@ from unittest import mock
 
 import pytest
 
-from product import add_product, update_product, delete_product
+from product import enumerate_product,add_product, update_product, delete_product
 
 
 @pytest.fixture
@@ -13,6 +13,12 @@ def product_list_with_no_products():
 def product_list_with_item():
     return [{"Pear", 300.00}]
 
+def test_prod_enumerate(product_list_with_item):
+
+    expected_outcome = "Number: 0, {'Product': 'Pear', 'Price': '300.0'}"
+
+    answer = enumerate_product(product_list_with_item)
+    assert answer == expected_outcome
 
 def test_product_addition(product_list_with_no_products):
     # set up
@@ -42,16 +48,16 @@ def test_prod_delete(product_list_with_item):
         assert product_list_with_item == expected_outcome
 
 
-def test_prod_change(product_list_with_item):
-    mock_index = 0
-    mock_new_product = "Potato"
-    mock_new_price = "1.15"
+# def test_prod_change(product_list_with_item):
+#     mock_index = 0
+#     mock_new_product = "Potato"
+#     mock_new_price = "1.15"
 
-    expected_outcome = [{"Product": "Potato", "Price": 1.15}]
+#     expected_outcome = [{"Product": "Potato", "Price": 1.15}]
 
-    mock_args = [mock_index, mock_new_product, mock_new_price]
-    with mock.patch("builtins.input") as mock_input:
-        mock_input.side_effect = mock_args
-        assert product_list_with_item == [{"Pear", 300}]
-        update_product(product_list_with_item)
-        assert product_list_with_item == expected_outcome
+#     mock_args = [mock_index, mock_new_product, mock_new_price]
+#     with mock.patch("builtins.input") as mock_input:
+#         mock_input.side_effect = mock_args
+#         assert product_list_with_item == [{"Pear", 300}]
+#         update_product(product_list_with_item)
+#         assert product_list_with_item == expected_outcome
